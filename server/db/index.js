5755b1679968e68eb596a90e145388c6c8eccb3d
Ian Zuber <ianzuber221@gmail.com>
2022-08-04 20:03:00 -0400

    Staged for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -5,6 +5,7 @@ const { Pool } = require('pg');
 // for connection information
 const pool = new Pool({
   user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
